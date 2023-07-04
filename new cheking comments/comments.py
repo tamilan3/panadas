690250a29d8f9b6d7c03f376ae6a763c0f835ca1
tamilan3 <tamilaninbathtamilan@gmail.com>
2023-07-04 11:29:19 +0530

    new flow

--- new cheking comments/comments.py
+++ new cheking comments/comments.py
@@ -4,7 +4,8 @@ read=pd.read_excel(file)
 read=read.drop(read.columns[[0,1,2,4,6,7]],axis=1)
 read=read.fillna("Not Commented")
 read=read[read['Comment']=='Not Commented']
-read.to_excel("Null_comment_list.xlsx",index=False)
-   
+#read.to_excel("Null_comment_list.xlsx",index=False)
+read=read.drop(read.iloc[-1].tolist())
+print(read)   
     
     
--- youtrack19/projectreport.py
+++ youtrack19/projectreport.py
@@ -1,14 +1,22 @@
 import pandas as pd
-file=("/home/onedata/Downloads/panadas/youtrack19/project.xlsx")
-new=pd.read_excel(file)
-new1=new.drop(columns=["Estimation time"])
-print(new)
-new1["Spent time"]=new["Spent time"].div(60).round(0)
-new["Spent time"]=new["Spent time"]%60
-new1=new1.astype({"Spent time":int})
-new1=new1.astype({"Spent time":str})
-new=new.astype({"Spent time":str})
-new=new.drop(columns=["Estimation time"])
-new["Spent time"]=new1["Spent time"]+"h."+new["Spent time"]+"m"
-new=new.rename(columns={"Item":"Projects"})
-print(new)
+try:
+    file=("/home/onedata/Downloads/panadas/youtrack19/project.xlsx")
+    new=pd.read_excel(file)
+    new1=new.drop(columns=["Estimation time"])
+    print(new)
+    new1["Spent time"]=new["Spent time"].div(60).round(0)
+    new["Spent time"]=new["Spent time"]%60
+    new1=new1.astype({"Spent time":int})
+    new1=new1.astype({"Spent time":str})
+    new=new.astype({"Spent time":str})
+    new=new.drop(columns=["Estimation time"])
+    new["Spent time"]=new1["Spent time"]+"h."+new["Spent time"]+"m"
+    new=new.rename(columns={"Item":"Projects"})
+    print(new)
+
+except FileNotFoundError:
+    print("File not Found")
+except FileExistsError:
+    print("File exist ")
+finally:
+    print("please give the necessary parameters")
